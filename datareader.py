1acbe118a29637e312e998c2896382305d72a767
nuriagomv <nuriagomv@gmail.com>
2019-04-04 20:11:55 +0200

    inicio

--- datareader.py
+++ datareader.py
@@ -4,9 +4,16 @@ import pandas as pd
 
 #lectura Google Analytics Catálogo EBA
 
-general=pd.read_excel(r'C:\Users\nurit\Dropbox\Estadísticas EBA Nuria\Mi trabajo\Google Analytics Catálogo EBA.xlsx',sheet_name='Visión general')
-print(general.columns)
+GAcatagolo=pd.ExcelFile(r'C:\Users\nurit\Dropbox\Estadísticas EBA Nuria\Mi trabajo\Google_Analytics-Catálogo_EBA.xlsx')
 
-usuariosActivos=pd.read_excel(r'C:\Users\nurit\Dropbox\Estadísticas EBA Nuria\Mi trabajo\Google Analytics Catálogo EBA.xlsx',sheet_name='Usuarios activos')
-print(usuariosActivos.columns)
+general = pd.read_excel(GAcatagolo,sheet_name='Visión general')
+usuariosActivos = pd.read_excel(GAcatagolo,sheet_name='Usuarios activos')
+nuevovsrecu = pd.read_excel(GAcatagolo,sheet_name='Nuevos vs. recu')
+frec = pd.read_excel(GAcatagolo,sheet_name='Frecuencia y asiduidad')
+inter = pd.read_excel(GAcatagolo,sheet_name='Interacción')
+demogr = pd.read_excel(GAcatagolo,sheet_name='Datos demográficos')
+ubi = pd.read_excel(GAcatagolo,sheet_name='Ubicación')
+tvida = pd.read_excel(GAcatagolo,sheet_name='Valor tiempo de vida cliente')
+
+#lectura Google Analytics Tienda EBA
 
