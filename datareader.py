6fd4e9a5b711826b9258da81302ba451eba88147
nuriagomv <nuriagomv@gmail.com>
2019-04-09 22:26:03 +0200

    lh

--- datareader.py
+++ datareader.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd 
 
 
-# lectura Google Analytics Catálogo EBA
+def loadData():
 
-def cargarGA():
+
+ # lectura Google Analytics Catálogo EBA
+
+   def loadGAcatalogo():
 
     GAcatagolo = pd.ExcelFile(r'Datos\Google_Analytics-Catálogo_EBA.xlsx')
 
@@ -18,64 +21,74 @@ def cargarGA():
     tvidac           = pd.read_excel(GAcatagolo,sheet_name='Valor tiempo de vida cliente')
 
 
-# lectura Google Analytics Tienda EBA
+ # lectura Google Analytics Tienda EBA
 
-GAtienda = pd.ExcelFile(r'Datos\Google_Analytics-Catálogo_EBA.xlsx')
+   def loadGAtienda():
 
-generalt         = pd.read_excel(GAtienda,sheet_name='Visión general')
-usuariosActivost = pd.read_excel(GAtienda,sheet_name='Usuarios activos')
-nuevovsrecut     = pd.read_excel(GAtienda,sheet_name='Nuevos vs. recu')
-frect            = pd.read_excel(GAtienda,sheet_name='Frecuencia y asiduidad')
-intert           = pd.read_excel(GAtienda,sheet_name='Interacción')
-demogrt          = pd.read_excel(GAtienda,sheet_name='Datos demográficos')
-ubit             = pd.read_excel(GAtienda,sheet_name='Ubicación')
-tvidat           = pd.read_excel(GAtienda,sheet_name='Valor tiempo de vida cliente')
+    GAtienda = pd.ExcelFile(r'Datos\Google_Analytics-Catálogo_EBA.xlsx')
 
-
-
-# lectura Facebook Analytics
-
-fbanalytics = pd.ExcelFile(r'Datos\Facebook_Analytics.xlsx')
+    generalt         = pd.read_excel(GAtienda,sheet_name='Visión general')
+    usuariosActivost = pd.read_excel(GAtienda,sheet_name='Usuarios activos')
+    nuevovsrecut     = pd.read_excel(GAtienda,sheet_name='Nuevos vs. recu')
+    frect            = pd.read_excel(GAtienda,sheet_name='Frecuencia y asiduidad')
+    intert           = pd.read_excel(GAtienda,sheet_name='Interacción')
+    demogrt          = pd.read_excel(GAtienda,sheet_name='Datos demográficos')
+    ubit             = pd.read_excel(GAtienda,sheet_name='Ubicación')
+    tvidat           = pd.read_excel(GAtienda,sheet_name='Valor tiempo de vida cliente')
 
 
-visitasf     = pd.read_excel(fbanalytics,sheet_name='Visitas a la página')
-retencionf  = pd.read_excel(fbanalytics,sheet_name='Retención de usuarios')
-postreacf    = pd.read_excel(fbanalytics,sheet_name='Post reactions por post')
-reacf        = pd.read_excel(fbanalytics,sheet_name='Reacciones a las publicaciones')
-compartidof  = pd.read_excel(fbanalytics,sheet_name='Publicaciones compartidas')
-comentariosf = pd.read_excel(fbanalytics,sheet_name='Comentarios en las publicacione')
-mensajesf    = pd.read_excel(fbanalytics,sheet_name='Mensajes enviados')
+ # lectura Facebook Analytics
+
+   def loadFBanalytics():
+
+    fbanalytics = pd.ExcelFile(r'Datos\Facebook_Analytics.xlsx')
+
+    visitasf     = pd.read_excel(fbanalytics,sheet_name='Visitas a la página')
+    retencionf  = pd.read_excel(fbanalytics,sheet_name='Retención de usuarios')
+    postreacf    = pd.read_excel(fbanalytics,sheet_name='Post reactions por post')
+    reacf        = pd.read_excel(fbanalytics,sheet_name='Reacciones a las publicaciones')
+    compartidof  = pd.read_excel(fbanalytics,sheet_name='Publicaciones compartidas')
+    comentariosf = pd.read_excel(fbanalytics,sheet_name='Comentarios en las publicacione')
+    mensajesf    = pd.read_excel(fbanalytics,sheet_name='Mensajes enviados')
 
 
-# lectura Píxel Analytics
 
-pixel = pd.ExcelFile(r'Datos\Píxel_Analytics.xlsx')
+ # lectura Píxel Analytics
 
-visitasp   = pd.read_excel(pixel,sheet_name='Visitas a la página')
-ingresos   = pd.read_excel(pixel,sheet_name='Ingresos')
-retencionp = pd.read_excel(pixel,sheet_name='Retención de usuarios')
-demogrp    = pd.read_excel(pixel,sheet_name='Datos demográficos')
+   def loadPixel():
 
+    pixel = pd.ExcelFile(r'Datos\Píxel_Analytics.xlsx')
 
-# lectura Wordpress Catálogo EBA
-
-Wcatalogo = pd.ExcelFile(r'Datos\Wordpress_Catálogo_EBA.xlsx')
-
-traficoc     = pd.read_excel(Wcatalogo,sheet_name='Tráfico')
-paginasc     = pd.read_excel(Wcatalogo,sheet_name='Entradas y páginas')
-paisesc      = pd.read_excel(Wcatalogo,sheet_name='Países')
-clicsc       = pd.read_excel(Wcatalogo,sheet_name='Clics')
-referenciasc = pd.read_excel(Wcatalogo,sheet_name='Referencias')
-tiendac      = pd.read_excel(Wcatalogo,sheet_name='Tienda')
+    visitasp   = pd.read_excel(pixel,sheet_name='Visitas a la página')
+    ingresos   = pd.read_excel(pixel,sheet_name='Ingresos')
+    retencionp = pd.read_excel(pixel,sheet_name='Retención de usuarios')
+    demogrp    = pd.read_excel(pixel,sheet_name='Datos demográficos')
 
 
-# lectura Wordpress Tienda EBA
+ # lectura Wordpress Catálogo EBA
 
-Wtienda = pd.ExcelFile(r'Datos\Wordpress_Tienda_EBA.xlsx')
+   def loadWcatalogo():
 
-traficot     = pd.read_excel(Wtienda,sheet_name='Tráfico')
-paginast     = pd.read_excel(Wtienda,sheet_name='Entradas y páginas')
-paisest      = pd.read_excel(Wtienda,sheet_name='Países')
-clicst       = pd.read_excel(Wtienda,sheet_name='Clics')
-referenciast = pd.read_excel(Wtienda,sheet_name='Referencias')
-tiendat      = pd.read_excel(Wtienda,sheet_name='Tienda')
+    Wcatalogo = pd.ExcelFile(r'Datos\Wordpress_Catálogo_EBA.xlsx')
+
+    traficoc     = pd.read_excel(Wcatalogo,sheet_name='Tráfico')
+    paginasc     = pd.read_excel(Wcatalogo,sheet_name='Entradas y páginas')
+    paisesc      = pd.read_excel(Wcatalogo,sheet_name='Países')
+    clicsc       = pd.read_excel(Wcatalogo,sheet_name='Clics')
+    referenciasc = pd.read_excel(Wcatalogo,sheet_name='Referencias')
+    tiendac      = pd.read_excel(Wcatalogo,sheet_name='Tienda')
+
+
+ # lectura Wordpress Tienda EBA
+
+
+   def loadWtienda():
+
+    Wtienda = pd.ExcelFile(r'Datos\Wordpress_Tienda_EBA.xlsx')
+
+    traficot     = pd.read_excel(Wtienda,sheet_name='Tráfico')
+    paginast     = pd.read_excel(Wtienda,sheet_name='Entradas y páginas')
+    paisest      = pd.read_excel(Wtienda,sheet_name='Países')
+    clicst       = pd.read_excel(Wtienda,sheet_name='Clics')
+    referenciast = pd.read_excel(Wtienda,sheet_name='Referencias')
+    tiendat      = pd.read_excel(Wtienda,sheet_name='Tienda')
